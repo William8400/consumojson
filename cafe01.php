4196d71873134624c562dae592595d0750fbeac9
William8400 <wdonato02@gmail.com>
2025-10-20 17:26:17 -0300

    .

---
+++ cafe01.php
@@ -0,0 +1,24 @@
+<?php
+
+$url = "api-cafe01.json?nome=paçoca=pudim";
+
+$resposta = file_get_contents($url);
+
+$paçocas = json_decode($resposta, true);
+
+$nome = $_GET['nome'];
+
+switch($pacoca_especifica){
+
+    case "pudim":
+        $paçoca_pudim = $paçocas['paçocas']['paçoca de pudim'];
+        echo json_decode($paçocas);
+        break;
+
+    default:
+        echo json_encode($paçocas);
+        break;
+}
+
+
+
