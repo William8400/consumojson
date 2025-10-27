e39276a53aa21d456531cea47a5928db8d708686
William8400 <wdonato02@gmail.com>
2025-10-27 17:15:37 -0300

    terminando a atividade

--- emerson-paes.php
+++ emerson-paes.php
@@ -4,9 +4,16 @@ $url = "http://localhost/consumojson/api.php?paes=doce";
 
 $resposta = file_get_contents($url);
 
-echo $resposta;
+$pães = json_decode($resposta, true);
 
-$paes = json_decode($resposta, true);
+
+echo "<p><b>Nome do pão:</b> ".$pães['nome']."</p>";
+
+echo "<p><b>Tipo:</b> ".$pães['Tipo']."</p>";
+
+echo "<p><b>Origem:</b> ".$pães['Origem']."</p>";
+
+echo "<p><b>Nutrientes:</b> ".$pães['Nutrientes']."</p>";
 
 
 
